51779efbb3da6f361a5f17139876a69ee4494169
Ersun Warncke <ersun@freshrealm.co>
2016-02-26 19:05:25 -0800

    delete moved files

--- sql/log/01-response-finish.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `responseFinish` (
-  `requestId` binary(16) NOT NULL,
-  `responseFinishCreateTime` datetime(6) NOT NULL,
-  PRIMARY KEY (`requestId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- sql/log/01-user-agent.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `userAgent` (
-  `userAgentId` binary(16) NOT NULL,
-  `userAgentName` varchar(2048) DEFAULT NULL,
-  PRIMARY KEY (`userAgentId`),
-  KEY `userAgentName` (`userAgentName`(256))
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
